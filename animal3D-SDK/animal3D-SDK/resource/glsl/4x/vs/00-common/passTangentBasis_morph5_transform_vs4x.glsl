b3c6f5c340a642200fc067d18e79857daf8bc5d8
Aidan Murphy <aidan.murphy@mymail.champlain.edu>
2021-09-14 11:10:35 -0400

    Adding Interpolation and morph targets

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/vs/00-common/passTangentBasis_morph5_transform_vs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/vs/00-common/passTangentBasis_morph5_transform_vs4x.glsl
@@ -24,11 +24,23 @@
 
 #version 450
 
+struct sMorphTarget
+{
+	vec4 position, normal, tangent;
+};
+
+
+
+layout (location = 0) in sMorphTarget aMorphTarget[5];
+/*
 layout (location = 0) in vec4 aPosition;
 layout (location = 1) in vec4 aNormal;		// usually 2
 layout (location = 2) in vec4 aTangent;		// usually 10
 layout (location = 2) in vec4 aBitangent;	// usually 11
+*/
 layout (location = 15) in vec4 aTexcoord;	// usually 8
+
+
 
 uniform mat4 uP;
 uniform mat4 uMV, uMV_nrm;
@@ -48,13 +60,70 @@ vec4 nlerp(in vec4 v0, in vec4 v1, in float u);
 vec4 CatmullRom(in vec4 vP, in vec4 v0, in vec4 v1, in vec4 vN, in float u);
 vec4 nCatmullRom(in vec4 vP, in vec4 v0, in vec4 v1, in vec4 vN, in float u);
 
+sMorphTarget lerp(in sMorphTarget m0, in sMorphTarget m1, in float u)
+{
+	sMorphTarget m;
+	m.position = lerp(m0.position, m1.position, u);
+	m.normal = nlerp(m0.normal, m1.normal, u);
+	m.tangent = nlerp(m0.tangent, m1.tangent, u);
+
+	return m;
+}
+
+sMorphTarget CatmullRom(in sMorphTarget vP, in sMorphTarget v0, in sMorphTarget v1, in sMorphTarget vN, in float u)
+{
+	sMorphTarget m;
+	m.position = CatmullRom(vP.position, v0.position, v1.position, vN.position, u);
+	m.normal = nCatmullRom(vP.normal, v0.normal, v1.normal, vN.normal, u);
+	m.tangent = nCatmullRom(vP.tangent, v0.tangent, v1.tangent, vN.tangent, u);
+	return m;
+}
+
 void main()
 {
 	// DUMMY OUTPUT: directly assign input position to output position
 //	gl_Position = aPosition;
 
-	vTangentBasis_view = uMV_nrm * mat4(aTangent, aBitangent, aNormal, vec4(0.0));
-	vTangentBasis_view[3] = uMV * aPosition;
+	float t = float(uTime * 1.5);
+	float u = fract(t);
+	int i0 = int(t) % 5;
+	int i1 = (i0 + 1) % 5;
+	int iN = (i1 + 1) % 5;
+	int iP = (i0 + 4) % 5;
+
+	sMorphTarget k;
+	sMorphTarget k0 = aMorphTarget[i0];
+	sMorphTarget k1 = aMorphTarget[i1];
+
+	sMorphTarget kN = aMorphTarget[iN];
+	sMorphTarget kP = aMorphTarget[iP];
+
+	k = aMorphTarget[i0];
+
+	//Step
+	k = k0;
+
+	//nearest
+	k = u < 0.5 ? k : k1;
+
+	//lerp
+	k = lerp(k0, k1, u);
+
+	//CatmullRom
+	k = CatmullRom(kP, k0, k1, kN, u);
+
+	vTangentBasis_view = uMV_nrm * mat4(
+	k.tangent, 
+	vec4(cross(k.normal.xyz, k.tangent.xyz), 0.0), 
+	k.normal, 
+	vec4(0.0));
+	
+	vTangentBasis_view[3] = uMV * k.position;
+
+	//vTangentBasis_view = uMV_nrm * mat4(aTangent, aBitangent, aNormal, vec4(0.0));
+	//vTangentBasis_view[3] = uMV * aPosition;
+
+
 	gl_Position = uP * vTangentBasis_view[3];
 	
 	vTexcoord_atlas = uAtlas * aTexcoord;
