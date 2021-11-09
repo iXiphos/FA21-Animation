ef726ec57d66455bcded9d684066a612823c6c3e
Aidan Murphy <44793359+iXiphos@users.noreply.github.com>
2021-11-09 01:04:17 -0500

    Pushing input stuff

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode1_Animation/a3_DemoMode1_Animation-idle-update.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode1_Animation/a3_DemoMode1_Animation-idle-update.c
@@ -187,30 +187,31 @@ void a3animation_update(a3_DemoState* demoState, a3_DemoMode1_Animation* demoMod
 			demoMode->pos.y = demoMode->obj_skeleton_ctrl->position.y + (a3f32)(demoState->xcontrol->ctrl.lThumbY_unit * dt);
 			break;
 		case animation_input_interpolate1:
-			demoMode->pos.x = demoMode->obj_skeleton_ctrl->position.x + (a3f32)((demoState->xcontrol->ctrl.lThumbX_unit - demoMode->obj_skeleton_ctrl->position.x)* dt);
-			demoMode->pos.y = demoMode->obj_skeleton_ctrl->position.y + (a3f32)((demoState->xcontrol->ctrl.lThumbY_unit - demoMode->obj_skeleton_ctrl->position.y)* dt);
+			demoMode->pos.x = demoMode->obj_skeleton_ctrl->position.x + (a3f32)((demoState->xcontrol->ctrl.lThumbX_unit - demoMode->obj_skeleton_ctrl->position.x)/ dt);
+			demoMode->pos.y = demoMode->obj_skeleton_ctrl->position.y + (a3f32)((demoState->xcontrol->ctrl.lThumbY_unit - demoMode->obj_skeleton_ctrl->position.y)/ dt);
 			break;
 		case animation_input_interpolate2:
 			//This needs to be velocity interpolate
 			break;
 		case animation_input_kinematic:
 			//https://openstax.org/books/university-physics-volume-1/pages/3-6-finding-velocity-and-displacement-from-acceleration
-			demoMode->pos.x = demoMode->obj_skeleton_ctrl->position.x + (a3f32)((demoMode->vel.x + demoState->xcontrol->ctrl.lThumbX_unit) * dt) + (a3f32)(0.5f * demoState->xcontrol->ctrl.lThumbX_unit * (dt * dt));
-			demoMode->pos.y = demoMode->obj_skeleton_ctrl->position.y + (a3f32)((demoMode->vel.y + demoState->xcontrol->ctrl.lThumbY_unit) * dt) + (a3f32)(0.5f * demoState->xcontrol->ctrl.lThumbY_unit * (dt * dt));
+			demoMode->vel.x = (a3f32)(100 * demoState->xcontrol->ctrl.lThumbX_unit * dt);
+			demoMode->pos.x = demoMode->obj_skeleton_ctrl->position.x + (a3f32)(demoMode->vel.x * dt) + (a3f32)(0.5f * demoState->xcontrol->ctrl.lThumbX_unit * (dt * dt));
+			
+			demoMode->vel.y = (a3f32)(100 * demoState->xcontrol->ctrl.lThumbY_unit * dt);
+			demoMode->pos.y = demoMode->obj_skeleton_ctrl->position.y + (a3f32)(demoMode->vel.y * dt) + (a3f32)(0.5f * demoState->xcontrol->ctrl.lThumbY_unit * (dt * dt));
 			break;
 		}
 
 		switch (demoMode->ctrl_rotation) {
 		case animation_input_direct:
-			demoMode->rot = (a3f32)demoState->xcontrol->ctrl.rThumbX_unit;
+			demoMode->rot = (a3f32)demoState->xcontrol->ctrl.rThumbX_unit * 180.0f;
 			break;
 		case animation_input_euler:
-			demoMode->pos.x = demoMode->obj_skeleton_ctrl->position.x + (a3f32)(demoState->xcontrol->ctrl.lThumbX_unit * dt);
-			demoMode->pos.y = demoMode->obj_skeleton_ctrl->position.y + (a3f32)(demoState->xcontrol->ctrl.lThumbY_unit * dt);
+			demoMode->velr = (a3f32)demoState->xcontrol->ctrl.rThumbX_unit * 180.0f;
+			demoMode->rot = demoMode->obj_skeleton_ctrl->euler.z + (a3f32)(demoMode->velr * dt);
 			break;
 		case animation_input_interpolate1:
-			demoMode->pos.x = demoMode->obj_skeleton_ctrl->position.x + (a3f32)((demoState->xcontrol->ctrl.lThumbX_unit - demoMode->obj_skeleton_ctrl->position.x) * dt);
-			demoMode->pos.y = demoMode->obj_skeleton_ctrl->position.y + (a3f32)((demoState->xcontrol->ctrl.lThumbY_unit - demoMode->obj_skeleton_ctrl->position.y) * dt);
 			break;
 		case animation_input_interpolate2:
 			//This needs to be velocity interpolate
